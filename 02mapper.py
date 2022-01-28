034221a8c757da2fdc1063030836d5ecc78bf018
s541910 <s541910@nwmissouri.edu>
2022-01-28 14:47:14 -0600

    added 02mapper

---
+++ 02mapper.py
@@ -0,0 +1,17 @@
+#Ramu Vallapurapu
+#Practice Mapper
+
+
+
+f = open("purchases.txt","r")  # open file, read-only
+o = open("locationresult02.txt", "w") # open file, write
+for line in f:  
+    rowList = line.strip().split("    ") 
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(amount, location))
+f.close()
+o.close()
